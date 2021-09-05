33c95f9d820941b42363708a0c19562f3ad7887c
JosueMartinez <josuemartinez.cr@gmail.com>
2021-09-05 00:19:17 -0600

    Loggin basico html

--- BSL Web/karma.conf.js
+++ BSL Web/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Banco'),
+      dir: require('path').join(__dirname, './coverage/BSL'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ BSL Web/src/app/loggin/body-loggin/body-loggin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-loggin',
+  templateUrl: './body-loggin.component.html',
+  styleUrls: ['./body-loggin.component.scss']
+})
+export class BodyLogginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
