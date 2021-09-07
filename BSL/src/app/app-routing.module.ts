23e44300363cb548d0371a7b791206f183af7620
JosueMartinez <josuemartinez.cr@gmail.com>
2021-09-07 10:13:03 -0600

    Loggin Oficial

--- BSL/src/app/app-routing.module.ts
+++ BSL/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogginComponent } from './loggin/loggin.component';
-import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   {path:'loggin',
   component:LogginComponent
   },
-  {
-    path:'home',
-    component:HomeComponent
-  }
+
+  { path: 'admin', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'notFound', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
 
 @NgModule({
